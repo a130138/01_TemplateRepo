52a694e6188ecc3d4aa72f58729b213595da0e43
a130138 <a130138@amerisourcebergen.com>
2022-10-12 15:19:15 -0400

    fw setup

---
+++ 02_AzFirewall/01_Deploy.ps1
@@ -0,0 +1,22 @@
+# AZ Firewall Creation
+# Clear Host
+    Clear-Host
+# Connect to AZ account
+    Connect-AzAccount
+# Change to appropriate subscription
+    Set-AzContext "239c71f8-a24e-41d1-96f6-b4f834a1f615"
+# Variables
+    $ResourceGroupName = "JoePOC1"
+
+# Execute
+    New-AzResourceGroupDeployment `
+        -ResourceGroupName $ResourceGroupName `
+        -TemplateUri "" `
+        -TemplateParameterUri "" `
+
+
+
+
+# Clean Up
+#   Get-AzF -ResourceGroupName JoePOC1
+#   Remove-AzF
