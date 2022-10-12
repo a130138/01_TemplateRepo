12ac673ed021938c6229c45d3dc717e1cec28aae
a130138 <a130138@amerisourcebergen.com>
2022-10-12 16:00:32 -0400

    updated to fw policy

--- 02_AzFirewall/01_FirewallPolicy/01_Deploy.ps1
+++ 02_AzFirewall/01_FirewallPolicy/01_Deploy.ps1
@@ -7,16 +7,12 @@
     Set-AzContext "239c71f8-a24e-41d1-96f6-b4f834a1f615"
 # Variables
     $ResourceGroupName = "JoePOC1"
-
 # Execute
     New-AzResourceGroupDeployment `
         -ResourceGroupName $ResourceGroupName `
         -TemplateUri "" `
         -TemplateParameterUri "" `
 
-
-
-
 # Clean Up
 #   Get-AzF -ResourceGroupName JoePOC1
-#   Remove-AzF
+#   Remove-AzF -ResourceGroupName JoePOC1
