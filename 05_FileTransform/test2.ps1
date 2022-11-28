426eeb16cc426b471b57c07ee7fd167bf80a11ca
a130138 <112408574+a130138@users.noreply.github.com>
2022-11-28 13:58:26 -0500

    test

--- 05_FileTransform/test2.ps1
+++ 05_FileTransform/test2.ps1
@@ -1,2 +1,3 @@
+Write-Host "Replacing variables and saving to new file"
 (Get-Content $env:System_DefaultWorkingDirectory\05_FileTransform\test.txt) -replace 'environment','POC' | Out-File $env:System_DefaultWorkingDirectory\05_FileTransform\NewFile1.txt
 Get-Content $env:System_DefaultWorkingDirectory\05_FileTransform\NewFile1.txt
