2cd821209a52366296da666c8fd5262fe819754b
a130138 <112408574+a130138@users.noreply.github.com>
2022-11-28 13:28:20 -0500

    test

--- 05_FileTransform/test2.ps1
+++ 05_FileTransform/test2.ps1
@@ -1,1 +1,2 @@
-(Get-Content $env:System_DefaultWorkingDirectory\05_FileTransform\test.txt) -replace 'environment','POC' | Out-File $env:System_DefaultWorkingDirectory\05_FileTransform\NewFile.txt
+(Get-Content $env:System_DefaultWorkingDirectory\05_FileTransform\test.txt) -replace 'environment','POC' | Out-File $env:System_DefaultWorkingDirectory\05_FileTransform\NewFile1.txt
+Get-Content $env:System_DefaultWorkingDirectory\05_FileTransform\NewFile1.txt
