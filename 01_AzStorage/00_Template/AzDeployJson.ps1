18234f161ea0b972ed3eb44d9eb80c76dbb9dbe3
a130138 <112408574+a130138@users.noreply.github.com>
2022-12-07 12:39:55 -0500

    Add files via upload

---
+++ 01_AzStorage/00_Template/AzDeployJson.ps1
@@ -0,0 +1,39 @@
+﻿### Azure Deploy (azuredeploy.json) ###
+
+# Clear-Host
+    Clear-Host
+
+# Setup
+    ## Variables
+        ### Date/Time
+	        $TimeStamp = Get-Date -Format "MMddyyyhhmm"
+        ### Input Azure Item To Be Created (start with Az)
+            $AzItem = "AzStorageAcct"
+	    ### Repo Info
+            $Repo = "C:\TEMP"
+            $TemplateFolder = "$Repo\01_AzTemplate"
+	        $TemplateFile = "$TemplateFolder\azuredeploy.json"
+        ### Source 
+            $SourceFolder = ""
+            $SourceFile = ""
+        ### Destination
+            $DestinationFile = "$Repo\$AzItem\azuredeploy.json"
+
+# Process
+    ## Create New Directory With Template File
+        New-Item -Path $Repo -ItemType Directory -Name $AzItem
+        Copy-Item $TemplateFile -Destination $Repo\$AzItem
+
+    ## Update File
+	    (Get-Content $DestinationFile) | Foreach-Object {
+			    $_ -replace 'AzName', 'testname' `
+			       -replace 'AzTagEnv', 'POC' `
+			       -replace 'AzTagOwn', 'jc' `
+			       -replace 'AzSkuName', 'Premium_LRS'  `
+                   -replace 'AzSkuTier','Premium' `
+                   -replace 'AzLoc','East US 2' `
+                   -replace 'AzKind','Storage' `
+			    } | Set-Content $DestinationFile
+
+    ## Read Updated File
+	    Get-Content $DestinationFile
---
+++ 01_AzStorage/00_Template/AzDeployYml.ps1
@@ -0,0 +1,22 @@
+﻿# Azure Deploy Yml Setup
+
+# Clear-Host
+	Clear-Host
+
+# Variables
+	$timestamp = Get-Date -Format "MMddyyyhhmm"
+	$repo = "C:\TEMP"
+	$source = "$Repo\Test.txt"
+	$destination = "$repo\$AzUnit.$timestamp.txt"
+
+
+# Process
+	(Get-Content $source) | Foreach-Object {
+			$_ -replace 'timestamp', $timestamp `
+			   -replace 'environment', 'POC' `
+			   -replace 'owner', 'jc' `
+			   -replace 'unit', $AzUnit `
+			} | Set-Content $destination
+
+
+	Get-Content $destination
