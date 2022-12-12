adcc629ddb4ef291a119758ad01218d57ff37bf0
a130138 <112408574+a130138@users.noreply.github.com>
2022-12-12 10:09:10 -0500

    testers

--- 01_AzStorage/01_AzStorageAccount/00_Template/AzDeploySetup2.ps1
+++ 01_AzStorage/01_AzStorageAccount/00_Template/AzDeploySetup2.ps1
@@ -10,19 +10,20 @@
 	        $TimeStamp = Get-Date -Format "MMddyyyhhmm"
         ### Input Az Item To Be Created (start with Az)
             $AzItem = "AzStorageAcct"
+            $AzCategory = "AzStorage"
 	    ### Repo Info
-            $AzRepo = "D:\a\r1\a\_a130138_01_TemplateRepo\01_AzStorage\01_AzStorageAccount"
+            $AzRepoFolder = "D:\a\r1\a\_a130138_01_TemplateRepo\01_$AzCategory\01_$AzItem"
             $AzTemplateFolder = "00_Template"
 	        $AzTemplateFileJSON = "azuredeploy.json"
             $AzTemplateFileYAML = "azure-pipelines.yml"
         ### Destination
-            $AzDestinationFileJSON = "$AzRepo\$AzItem$TimeStamp\$AzTemplateFileJSON"
-            $AzDestinationFileYAML = "$AzRepo\$AzItem$TimeStamp\$AzTemplateFileYAML"
+            $AzDestinationFileJSON = "$AzRepoFolder\$AzItem$TimeStamp\$AzTemplateFileJSON"
+            $AzDestinationFileYAML = "$AzRepoFolder\$AzItem$TimeStamp\$AzTemplateFileYAML"
 # Process
     ## JSON
         ### Create New Directory and Import Copy of JSON Template
-            New-Item -Path $AzRepo -ItemType Directory -Name $AzItem$TimeStamp
-            Copy-Item $AzRepo\$AzTemplateFolder\$AzTemplateFileJSON -Destination $AzRepo\$AzItem$TimeStamp
+            New-Item -Path $AzRepoFolder -ItemType Directory -Name $AzItem$TimeStamp
+            Copy-Item $AzRepoFolder\$AzTemplateFolder\$AzTemplateFileJSON -Destination $AzRepoFolder\$AzItem$TimeStamp
         ### Update JSON
             (Get-Content $AzDestinationFileJSON) | Foreach-Object {
                 $_ -replace '@@AzName@@',$env:AzName `
@@ -37,7 +38,7 @@
             Get-Content $AzDestinationFileJSON
     ## YAML
         ## Import copy of YAML into newly created Directory
-            Copy-Item $AzRepo\$AzTemplateFolder\$AzTemplateFileYAML -Destination $AzRepo\$AzItem$TimeStamp
+            Copy-Item $AzRepoFolder\$AzTemplateFolder\$AzTemplateFileYAML -Destination $AzRepoFolder\$AzItem$TimeStamp
         ## Update YAML
             (Get-Content $AzDestinationFileYAML) | Foreach-Object {
                 $_ -replace '@@AzScope@@',$env:AzScope `
