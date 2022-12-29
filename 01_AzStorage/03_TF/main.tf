2f07445af3a64768ab90f770e3e76797ca67aa3b
a130138 <112408574+a130138@users.noreply.github.com>
2022-12-29 13:55:52 -0500

    test

--- 01_AzStorage/03_TF/main.tf
+++ 01_AzStorage/03_TF/main.tf
@@ -1,18 +1,15 @@
-resource "azurerm_resource_group" "example"
-  {
-    name     = "example-resources"
-    location = "West Europe"
-  }
-
-resource "azurerm_storage_account" "example"
-  {
-    name                     = "storageaccountname"
+resource "azurerm_resource_group" "example"  {
+    name     = "JoePOC1"
+    location = "East US 2"
+}
+resource "azurerm_storage_account" "example"  {
+    name                     = "jcstoracct1"
     resource_group_name      = azurerm_resource_group.example.name
     location                 = azurerm_resource_group.example.location
     account_tier             = "Standard"
     account_replication_type = "GRS"
 
     tags = {
-      environment = "staging"
+      environment = "POC"
     }
-  }
+}
