78bbd89bcb47123f7f48a503e37f381fe883a627
a130138 <a130138@amerisourcebergen.com>
2022-12-29 16:17:28 -0500

    test

--- 01_AzStorage/03_TF/main.tf
+++ 01_AzStorage/03_TF/main.tf
@@ -6,18 +6,18 @@ resource "azurerm_subscription" "example" {
   subscription_name = "Dev_Azure_10002_CloudSandbox3"
   subscription_id   = "239c71f8-a24e-41d1-96f6-b4f834a1f615"
 }
-resource "azurerm_resource_group" "example"  {
-    name     = "JoePOC1"
-    location = "East US 2"
+resource "azurerm_resource_group" "example" {
+  name     = "JoePOC1"
+  location = "East US 2"
 }
-resource "azurerm_storage_account" "example"  {
-    name                     = "jcstoracct1"
-    resource_group_name      = azurerm_resource_group.example.name
-    location                 = azurerm_resource_group.example.location
-    account_tier             = "Standard"
-    account_replication_type = "GRS"
+resource "azurerm_storage_account" "example" {
+  name                     = "jcstoracct1"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
-    tags = {
-      environment = "POC"
-    }
+  tags = {
+    environment = "POC"
+  }
 }
