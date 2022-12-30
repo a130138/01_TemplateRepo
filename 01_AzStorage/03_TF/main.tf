080c884bce249817187c24dfc2a7c6018434edea
a130138 <112408574+a130138@users.noreply.github.com>
2022-12-30 11:47:49 -0500

    test

--- 01_AzStorage/03_TF/main.tf
+++ 01_AzStorage/03_TF/main.tf
@@ -1,3 +1,19 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "239c71f8-a24e-41d1-96f6-b4f834a1f615"
+}
 resource "azure_resource_group" "JoePOC1" {
    name = "JoePOC1"
    location = "East US 2"
