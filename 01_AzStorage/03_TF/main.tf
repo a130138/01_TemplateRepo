137855d7245e7d9e90515149218e2edf468a5ede
a130138 <112408574+a130138@users.noreply.github.com>
2022-12-30 10:15:37 -0500

    test

--- 01_AzStorage/03_TF/main.tf
+++ 01_AzStorage/03_TF/main.tf
@@ -1,19 +1,10 @@
 provider "azurerm" {
   features {}
 }
-resource "azurerm_subscription" "example" {
-  alias             = "examplesub"
-  subscription_name = "Dev_Azure_10002_CloudSandbox3"
-  subscription_id   = "239c71f8-a24e-41d1-96f6-b4f834a1f615"
-}
-resource "azurerm_resource_group" "example" {
-  name     = "JoePOC1"
-  location = "East US 2"
-}
 resource "azurerm_storage_account" "example" {
   name                     = "jcstoracct1"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = "JoePOC1"
+  location                 = "East US 2"
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
