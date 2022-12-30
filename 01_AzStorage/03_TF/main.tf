66cb228085aedd27107e041a1354bcb8ea514346
a130138 <112408574+a130138@users.noreply.github.com>
2022-12-30 11:38:32 -0500

    test

--- 01_AzStorage/03_TF/main.tf
+++ 01_AzStorage/03_TF/main.tf
@@ -4,8 +4,8 @@
 # }
 resource "azurerm_storage_account" "jc_storage_acct" {
   name                     = "jcstoracctname1"
-  resource_group_name      = JoePOC1
-  location                 = East US 2
+  resource_group_name      = "JoePOC1"
+  location                 = "East US 2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
